91f94490b8322f4007db1a73786c5aa20ae45f7a
MarcoHansen7 <s194302@student.dtu.dk>
2022-04-22 09:59:00 +0200

    putchar printchar

--- main.c
+++ main.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-void main() {
-    printf("\tC1\tC2\tC3\tC4\tC5\tC6\tC7\n\n");
-    printf("\t[]\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF1\n");
-    printf("\t[]\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF2\n");
-    printf("\t[]\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF3\n");
-    printf("\t[]\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF4\n");
-    printf("\t[]\t[]\t[]\t[]\t[]\t[]\t[]\n");
-    printf("\t[]\t[]\t[]\t[]\t[]\t[]\t[]\n");
-    printf("\t[]\t[]\t[]\n");
+int main(){
 
+    char c[2];
+
+    printf("Command: ");
+    c[0] = getchar();
+    c[1] = getchar();
+    printf("Character entered: ");
+    putchar(c[0]);
+    putchar(c[1]);
 
 
 }
+
+/*bool sameFace(card1,card2){
+    if (card1face == card2face){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+bool isSuitable(card1,card2){
+    if (card1number = card2number + 1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}*/
