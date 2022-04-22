9cfaeedd3b395fad83e8cfb524ae96538a8c55a5
anshjyotsingh <anshjyot1504@gmail.com>
2022-04-22 08:20:29 +0200

    First commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+    printf("\tC1\tC2\tC3\tC4\tC5\tC6\tC7\n\n");
+    printf("\t[]\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF1\n");
+    printf("\t[]\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF2\n");
+    printf("\t[]\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF3\n");
+    printf("\t[]\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF4\n");
+    printf("\t[]\t[]\t[]\t[]\t[]\t[]\t[]\n");
+    printf("\t[]\t[]\t[]\t[]\t[]\t[]\t[]\n");
+    printf("\t[]\t[]\t[]\n");
+
+
+
+}
